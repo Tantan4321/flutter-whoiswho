57421529311867cfb75373b66401c07f12ca6f90
Eastan Giebler <kupa528@yahoo.com>
2020-01-15 13:54:24 -0600

    Init flutter project

---
+++ android/app/src/main/kotlin/com/tantan4321/flutter_whoiswho/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tantan4321.flutter_whoiswho
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
