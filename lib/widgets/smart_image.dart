2323dd5e638c5e0d7bdc2bfabc0291513a119c25
Eastan Giebler <kupa528@yahoo.com>
2020-01-20 00:47:36 -0600

    Fix image loading

--- lib/widgets/smart_image.dart
+++ lib/widgets/smart_image.dart
@@ -6,31 +6,25 @@ class SmartImage extends StatelessWidget {
   static final String urlPattern =
       r"(https?|http)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
   final match = RegExp(urlPattern, caseSensitive: false);
-  FadeInImage image;
 
-  SmartImage(this.file) {
-    var firstmatch = match.firstMatch(this.file);
-    if (firstmatch != null) {
-      image = FadeInImage.assetNetwork(
-          fit: BoxFit.cover,
-          fadeInDuration: Duration(milliseconds: 100),
-          placeholder: "assets/img/error_image.jpg",
-          image: file);
-    } else if (file.startsWith("assets")) {
-      image = FadeInImage.assetNetwork(
-          fit: BoxFit.cover,
-          fadeInDuration: Duration(milliseconds: 100),
-          placeholder: "assets/img/error_image.jpg",
-          image: file);
-    }else{
-      image = FadeInImage.assetNetwork(
-          fit: BoxFit.cover,
-          image: "assets/img/error_image.jpg");
-    }
-  }
+  SmartImage(this.file);
 
   @override
   Widget build(BuildContext context) {
-    return image;
+    var firstmatch = match.firstMatch(this.file);
+    if (firstmatch != null) {
+      return FadeInImage.assetNetwork(
+          fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 100),
+          placeholder: "assets/img/error_image.jpg",
+          image: file);
+    } else if (file.startsWith("assets")){
+      return Image.asset(file, fit: BoxFit.cover);
+    }else{
+      return FadeInImage.assetNetwork(
+          fit: BoxFit.cover,
+          image: "assets/img/error_image.jpg");
+    }
+
   }
 }
