cf8fd66406036bebbe46653d0dcfa1638c9005da
Eastan Giebler <kupa528@yahoo.com>
2020-01-20 00:25:14 -0600

    Image loader improvements

--- lib/widgets/smart_image.dart
+++ lib/widgets/smart_image.dart
@@ -6,24 +6,31 @@ class SmartImage extends StatelessWidget {
   static final String urlPattern =
       r"(https?|http)://([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?";
   final match = RegExp(urlPattern, caseSensitive: false);
-  ImageProvider image;
+  FadeInImage image;
 
   SmartImage(this.file) {
     var firstmatch = match.firstMatch(this.file);
     if (firstmatch != null) {
-      image = NetworkImage(file);
+      image = FadeInImage.assetNetwork(
+          fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 100),
+          placeholder: "assets/img/error_image.jpg",
+          image: file);
     } else if (file.startsWith("assets")) {
-      image = AssetImage(file);
+      image = FadeInImage.assetNetwork(
+          fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 100),
+          placeholder: "assets/img/error_image.jpg",
+          image: file);
     }else{
-      image = AssetImage("assets/img/error_image.jpg");
+      image = FadeInImage.assetNetwork(
+          fit: BoxFit.cover,
+          image: "assets/img/error_image.jpg");
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Image(
-      image: image,
-      fit: BoxFit.cover,
-    );
+    return image;
   }
 }
